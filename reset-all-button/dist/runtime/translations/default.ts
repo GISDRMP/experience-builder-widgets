fd8cd70f0fb46f6b97a4d0898b4fee4b79af299d
ravk2001 <ravneetkathuria11@gmail.com>
2026-04-17 15:54:44 -0400

    Add files via upload

---
+++ reset-all-button/dist/runtime/translations/default.ts
@@ -0,0 +1,15 @@
+export default {
+  _widgetLabel: 'Reset All Button',
+  defaultButtonText: 'Reset All',
+  resetInProgress: 'Resetting…',
+  nothingConfigured: 'Configure at least one reset action in the widget settings.',
+  resetMap: 'Reset map extent',
+  clearSelection: 'Clear data source selections',
+  clearQuery: 'Clear data source query/filter state',
+  resetVisibleFilterWidgets: 'Reset visible Filter widgets',
+  clearVisibleListSelections: 'Clear visible List widgets',
+  buttonText: 'Button text',
+  mapWidget: 'Map widget',
+  dataSources: 'Data sources',
+  actions: 'Reset actions'
+}
---
+++ reset-all-button/dist/setting/translations/default.ts
@@ -0,0 +1,11 @@
+export default {
+  mapWidget: 'Map widget',
+  dataSources: 'Data sources to reset',
+  actions: 'Reset actions',
+  buttonText: 'Button text',
+  resetMap: 'Reset connected map extent/viewpoint',
+  clearSelection: 'Clear selected records on configured data sources',
+  clearQuery: 'Clear query/filter state on configured data sources',
+  resetVisibleFilterWidgets: 'Reset all visible Filter widgets in the page',
+  clearVisibleListSelections: 'Clear all visible List widget selections in the page'
+}
