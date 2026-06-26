9bf8a09bf1a747886fcc2165e1335f3e23ee4b3f
ravk2001 <ravneetkathuria11@gmail.com>
2026-06-26 16:29:15 -0400

    Add files via upload

---
+++ drmp-project-atlas/dist/runtime/translations/default.ts
@@ -0,0 +1,4 @@
+export default {
+  _widgetLabel: 'DRMP Project Atlas',
+  configureMapWidget: 'Select a Map widget in this widget\'s settings.'
+}
---
+++ drmp-project-atlas/dist/setting/translations/default.ts
@@ -0,0 +1,11 @@
+export default {
+  _widgetLabel: 'DRMP Project Atlas',
+  mapConnection: 'Map connection',
+  branding: 'Branding',
+  fieldMap: 'Project field map',
+  filterBar: 'Filter bar',
+  departments: 'Department toggles',
+  displayFields: 'List and project info fields',
+  proximity: 'Proximity and map behavior',
+  theme: 'Theme'
+}
